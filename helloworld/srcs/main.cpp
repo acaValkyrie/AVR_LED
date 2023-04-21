3538413c2d815f25b132b03878a3176b207b63c1
Yadokame <yadokame3.14@gmail.com>
2023-04-21 19:29:33 +0900

    move dir

--- helloworld/srcs/main.cpp
+++ helloworld/srcs/main.cpp
@@ -2,11 +2,11 @@
 
 int main(){
   DDRB = 0b00000001;
-
+  int delay_t_ms = 50 /*ms*/;
   while(1){
     PORTB = 0b00000001;
-    _delay_ms(1000);
+    _delay_ms(delay_t_ms);
     PORTB = 0b00000000;
-    _delay_ms(1000);
+    _delay_ms(delay_t_ms);
   }
 }
